45435c4cbfa1670cfbfc74e890e34a797f32b046
CameronT <TheJimmyBlaze@gmail.com>
2019-12-03 16:50:41 +1300

    Converted back to Razor
    Converted the project back to Razor. The ASP.net project is still there, just removed from solution. ASP required .core 3.1 in order to call components with args, and that was just too much of a pain to deal with, as 3.1 is still in preview atm.

---
+++ Blazor.Dev.Blog/Models/Profile.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Dev.Blog.Models
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+        public string ImageName { get; set; }
+        public string Blurb { get; set; }
+
+        public string Email { get; set; }
+        public string Github { get; set; }
+        public string LinkedIn { get; set; }
+
+        public IWebHostEnvironment Environment { get; set; }
+
+        public string GetImagePath()
+        {
+            const string COLLECT_ERROR = "Failed to collect Profile Image!";
+            const string IMAGE_PATH = @"images\profiles";
+
+            if (string.IsNullOrEmpty(ImageName))
+                throw new ArgumentException(string.Format("{0} Image path was empty for profile: {1}", COLLECT_ERROR, Name));
+
+            string path = Path.Combine(IMAGE_PATH, ImageName);
+            if (!File.Exists(Path.Combine(Environment.WebRootPath, path)))
+                throw new FileNotFoundException(string.Format("{0} Image path: {1} could not be found for profile: {1}", COLLECT_ERROR, path, Name));
+
+            return path;
+        }
+
+        public string GetMailToAddress()
+        {
+            const string MAIL_TO = "mailto";
+            return string.Format("{0}:{1}", MAIL_TO, Email);
+        }
+    }
+}
